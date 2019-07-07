09de2fdcbb528264c46b000abae738a7d6fcac29
rage5474 <mail@raphaelgeissler.de>
2019-07-07 09:10:10 +0000

    Fixing pylint issues.

--- test_main.py
+++ test_main.py
@@ -1,5 +1,10 @@
+"""This is only an example."""
+
+
 def main():
+    """This funtion prints Hello World!"""
     print("Hello World!")
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     main()
