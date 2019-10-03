057efeab9eaafdd4947dca929f4c9c225d52d218
rage5474 <mail@raphaelgeissler.de>
2019-10-03 06:44:18 +0000

    Using prebuild python363

--- test_main.py
+++ test_main.py
@@ -1,10 +1,9 @@
 """This is only an example."""
-
+import sys
 
 def main():
     """This funtion prints Hello World!"""
-    print("Hello World!")
-
+    print("Your using python version: " + sys.version)
 
 if __name__ == "__main__":
     main()
